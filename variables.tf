eb08012213b9efb29370c36ee87e01ae2a39d84c
Bommasani Vamsi Krishna <vamsikrishnab1992@gmail.com>
2025-04-25 23:58:34 -0400

    added ecr code for provisioning

--- variables.tf
+++ variables.tf
@@ -5,7 +5,6 @@
 variable "aws_region" {
   description = "provide the aws_region"
   type        = string
-  default     = "us-east-1"
 }
 
 # ======================================================
@@ -15,25 +14,21 @@ variable "aws_region" {
 variable "app_id" {
   description = "provide an app-id"
   type        = string
-  default     = "0312"
 }
 
 variable "environment" {
   description = "provide some environment name"
   type        = string
-  default     = "develop"
 }
 
 variable "mail" {
   description = "provide an email to send mails"
   type        = string
-  default     = "vamsikrishnab1992@gmail.com"
 }
 
 variable "name" {
   description = "provide an email to send mails"
   type        = string
-  default     = "vamsi"
 }
 
 # ======================================================
@@ -43,5 +38,4 @@ variable "name" {
 variable "number_of_images" {
   type        = number
   description = "number of images to keep in the ECR"
-  default     = 5
 }
